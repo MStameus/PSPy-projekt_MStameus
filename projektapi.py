702c4dd47bc1ed9b654f71fc46defa5e096070df
MStameus <morris.stameus@hotmail.se>
2023-05-24 09:00:12 +0200

    added constant heroes.json file from https://github.com/odota/dotaconstants to be enable the newly added method of setting 'heroname' for the player

--- projektapi.py
+++ projektapi.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # klass för spelare som kan initiaras med divierse stats från matchen
 class player:
-    def __init__(self, slot, gpm, xpm, kills, deaths, assists, networth):
+    def __init__(self, slot, gpm, xpm, kills, deaths, assists, networth, name):
         self.slot = slot
         self.gpm = gpm
         self.xpm = xpm
@@ -13,8 +13,11 @@ class player:
         self.deaths = deaths
         self.assists = assists
         self.networth = networth
+        self.name = name
     def __repr__(self):
         return repr(f'Player <slot> {self.slot}, <gpm> {self.gpm}, <xpm> {self.xpm}, <kills> {self.kills}, <deaths> {self.deaths}, <assists>, {self.assists}')
+    def setname(self, newname):
+        self.name = newname
 # Method for getting information from match and writing it on JSON file
 def fetchMatch(IDstring):
     response = requests.get (f'https://api.opendota.com/api/matches/{IDstring}') 
@@ -25,23 +28,33 @@ def fetchMatch(IDstring):
 
 # list wich will contain the players
 playerslist = []
+
 #Method for printing a basic scoreboard
 def printScoreboard():
     index = 1
     for player in playerslist:
-        print(f'Player{index}: GPM: {player.gpm}, XPM: {player.xpm}: KDA: {player.kills}/{player.deaths}/{player.assists} ')
+        print(f'Player{index}: {player.name} GPM: {player.gpm}, XPM: {player.xpm}: KDA: {player.kills}/{player.deaths}/{player.assists} ')
         index +=1
-# Method for initiating players from the match.json file
+
+# Method for initiating players from the match1.json file
 def initplayers():
     with open ('match1.json') as datafh:
          data = json.load(datafh)
     players = data['players']
     for key in players:
-        p = player(key['player_slot'], key['gold_per_min'], key['xp_per_min'], key['kills'], key['deaths'], key['assists'], key['net_worth']) #initierar 
+        p = player(key['player_slot'], key['gold_per_min'], key['xp_per_min'], key['kills'], key['deaths'], key['assists'], key['net_worth'], key['hero_id']) #initierar 
         playerslist.append(p)
 
+#method using static json file to set hero names for the players
+def setheronames():
+    with open ('heroes.json') as heroesfh:
+        heroes = json.load(heroesfh)
+    for player in playerslist:
+        hero = heroes[str(player.name)]
+        player.setname(hero['localized_name'])
+    
+
 def allPlayerGPMScatter(): 
-    # genereellt smålabbande kring matplotlib och hur spelares statistik kan användas för att visas upp i det 
     gpmlist = []
     amountplayers = []
     for i in range(len(playerslist)):
@@ -71,9 +84,8 @@ def notAllplayerGPMScatter(list):
         plt.annotate(label,(x,y))
     plt.show()
 
-
-def allPlayerXPMScatter(): 
-    # genereellt smålabbande kring matplotlib och hur spelares statistik kan användas för att visas upp i det 
+#
+def allPlayerXPMScatter():  
     xpmlist = []
     amountplayers = []
     for i in range(len(playerslist)):
@@ -139,6 +151,7 @@ printWelcome()
 inputMatchID = input('Ange matchid: ')
 fetchMatch(inputMatchID)
 initplayers()
+setheronames()
 printScoreboard()
 while True:
     commandLine = input('Ange statistik du vill se en scatterplot för: ').split(' ')
